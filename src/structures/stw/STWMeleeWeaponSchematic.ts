77cbdb197a7746a1b16de1a94a52fdb9c3cfa401
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-06-29 22:33:58 +0100

    add missing constructors

--- src/structures/stw/STWMeleeWeaponSchematic.ts
+++ src/structures/stw/STWMeleeWeaponSchematic.ts
@@ -1,9 +1,25 @@
 import STWWeaponSchematic from './STWWeaponSchematic';
-import type { STWSchematicMeleeSubType } from '../../../resources/structs';
+import { parseSTWSchematicTemplateId } from '../../util/Util';
+import type { STWSchematicEvoType, STWSchematicMeleeSubType } from '../../../resources/structs';
+import type { STWProfileSchematicData } from '../../../resources/httpResponses';
+import type Client from '../../Client';
 
 class STWMeleeWeaponSchematic extends STWWeaponSchematic {
-  public override type!: 'melee';
-  public override subType!: STWSchematicMeleeSubType;
+  public override type: 'melee';
+  public override subType: STWSchematicMeleeSubType;
+
+  constructor(client: Client, id: string, data: STWProfileSchematicData & {
+    type: 'melee';
+    subType: STWSchematicMeleeSubType;
+    evoType: STWSchematicEvoType;
+  }) {
+    super(client, id, data);
+
+    const parsedSchematic = parseSTWSchematicTemplateId(data.templateId);
+
+    this.type = parsedSchematic.type as | 'melee';
+    this.subType = parsedSchematic.subType as STWSchematicMeleeSubType;
+  }
 }
 
 export default STWMeleeWeaponSchematic;
